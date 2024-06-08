30d9a4840fab2217e02108183b07a189e0b26ecb
Alondra Alvídrez <24.alvidrez.alondra@gmail.com>
2024-06-07 18:54:58 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -20,9 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const handleTransitionEnd = () => {
         if (slideIndex === 0) {
             slidesContainer.style.transition = 'none';
-            slideIndex = totalSlides - 2; // Move to the last real slide
+            
             slidesContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
-        } else if (slideIndex === totalSlides - 2 && slideIndex === totalSlides) {
+            slideIndex = totalSlides - 2; // Move to the last real slide
+        } else if (slideIndex === totalSlides - 2) {
             slidesContainer.style.transition = 'none';
             slideIndex = 1; // Move to the first real slide
             slidesContainer.style.transform = `translateX(-100%)`;
